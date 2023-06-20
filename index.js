90dba066271bdccbf5341e7a2d383e26ffdb3ac0
George <georgie.george7@gmail.com>
2023-06-20 12:56:24 +0400

    eslint update file

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const nav = document.querySelector('nav');
 const navicons = document.querySelectorAll('.navicons');
 const hamburger = document.querySelector('#hamburger');
 
-menutoggle.addEventListener("click", () => {
-    nav.classList.toggle('show');
-    navicons.forEach(icon => {
-        icon.classList.toggle('hidden');
-    });
+menutoggle.addEventListener('click', () => {
+  nav.classList.toggle('show');
+  navicons.forEach((icon) => {
+    icon.classList.toggle('hidden');
+  });
 });
 
-window.addEventListener("resize", () => {
-    if(document.body.clientWidth > 720){
-        nav.classList.remove('show');
-        navicons.forEach(icon => {
-            icon.classList.add('hidden')
-        });
-        hamburger.classList.remove('hidden')
-    }
+window.addEventListener('resize', () => {
+  if (document.body.clientWidth > 720) {
+    nav.classList.remove('show');
+    navicons.forEach((icon) => {
+      icon.classList.add('hidden');
+    });
+    hamburger.classList.remove('hidden');
+  }
 });
