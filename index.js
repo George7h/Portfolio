0d7128834f5694be00dbdc6eacc6cef90fd2254a
CuauCabrera <cabrera.cuau@gmail.com>
2023-06-20 02:31:10 -0600

    Add js events

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function toggleMenu(menu) {
-    menu.classList.toggle('open');
-}
+const menutoggle = document.querySelector('#menutoggle');
+const nav = document.querySelector('nav');
+const navicons = document.querySelectorAll('.navicons');
+const hamburger = document.querySelector('#hamburger');
+
+menutoggle.addEventListener("click", () => {
+    nav.classList.toggle('show');
+    navicons.forEach(icon => {
+        icon.classList.toggle('hidden');
+    });
+});
+
+window.addEventListener("resize", () => {
+    if(document.body.clientWidth > 720){
+        nav.classList.remove('show');
+        navicons.forEach(icon => {
+            icon.classList.add('hidden')
+        });
+        hamburger.classList.remove('hidden')
+    }
+});
