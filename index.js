bfd4b3bcdfb4b26661603eb9d39f4cf8df0f287a
George <georgie.george7@gmail.com>
2023-06-21 19:00:16 +0400

    add Form validation

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const nav = document.querySelector('nav');
 const navicons = document.querySelectorAll('.navicons');
 const hamburger = document.querySelector('#hamburger');
 
+
+
+
 menutoggle.addEventListener('click', () => {
   nav.classList.toggle('show');
   navicons.forEach((icon) => {
@@ -27,3 +30,56 @@ window.addEventListener('resize', () => {
     hamburger.classList.remove('hidden');
   }
 });
+
+
+
+// // From this point is to validate the form data
+
+
+
+// show a message with a type of the input
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#form1");
+
+const NAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter your address with lowercases";
+
