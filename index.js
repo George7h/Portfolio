97a1b0bf3463d1e0f071ee6bf730c103d22d7cc8
George <georgie.george7@gmail.com>
2023-06-21 21:04:56 +0400

    linter error

--- index.js
+++ index.js
@@ -4,94 +4,87 @@ const nav = document.querySelector('nav');
 const navicons = document.querySelectorAll('.navicons');
 const hamburger = document.querySelector('#hamburger');
 
-
-
-
 menutoggle.addEventListener('click', () => {
-	nav.classList.toggle('show');
-	navicons.forEach((icon) => {
-		icon.classList.toggle('hidden');
-	});
+  nav.classList.toggle('show');
+  navicons.forEach((icon) => {
+    icon.classList.toggle('hidden');
+  });
 });
 
 menutoggle2.addEventListener('click', () => {
-	nav.classList.toggle('show');
-	navicons.forEach((icon) => {
-		icon.classList.toggle('hidden');
-	});
+  nav.classList.toggle('show');
+  navicons.forEach((icon) => {
+    icon.classList.toggle('hidden');
+  });
 });
 
 window.addEventListener('resize', () => {
-	if (document.body.clientWidth > 768) {
-		nav.classList.remove('show');
-		navicons.forEach((icon) => {
-			icon.classList.add('hidden');
-		});
-		hamburger.classList.remove('hidden');
-	}
+  if (document.body.clientWidth > 768) {
+    nav.classList.remove('show');
+    navicons.forEach((icon) => {
+      icon.classList.add('hidden');
+    });
+    hamburger.classList.remove('hidden');
+  }
 });
-
-
 
 // // From this point is to validate the form data
 
-
-
 // show a message with a type of the input
 function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+  // get the small element and set the message
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? 'success' : 'error';
+  return type;
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+  return showMessage(input, message, false);
 }
 
 function showSuccess(input) {
-	return showMessage(input, "", true);
+  return showMessage(input, '', true);
 }
 
 function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
 }
 
 function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
+  const email = input.value.trim();
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
 }
 
-const form = document.querySelector("#form1");
+const form = document.querySelector('#form1');
 
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter your address with lowercases";
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const EMAIL_INVALID = 'Please enter your address with lowercases';
 
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
+form.addEventListener('submit', (event) => {
+  // stop form submission
+  event.preventDefault();
 
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		form.submit();
-	}
+  // validate the form
+  const nameValid = hasValue(form.elements.name, NAME_REQUIRED);
+  const emailValid = validateEmail(form.elements.email, EMAIL_REQUIRED, EMAIL_INVALID);
+  // if valid, submit the form.
+  if (nameValid && emailValid) {
+    form.submit();
+  }
 });
